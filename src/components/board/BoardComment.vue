b6dbfd0390fb234d89c35f745ff82dedc378ab53
sbsc20 <sbsc20@naver.com>
2022-11-15 17:57:18 +0900

    boardview

---
+++ src/components/board/BoardComment.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ comments }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BoardComment",
+  props: {
+    comments: [],
+  },
+};
+</script>
+
+<style></style>
