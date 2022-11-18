9c58dac1997cc6c2d03717c55197b9295fc54778
subinan <060657@naver.com>
2022-11-18 16:03:24 +0900

    no template

--- src/main.js
+++ src/main.js
@@ -5,24 +5,6 @@ import store from "./store";
 
 import "@/api/vueBootstrap.js";
 
-import "@/assets/plugins/themefisher-font/style.css";
-import "@/assets/plugins/bootstrap/css/bootstrap.min.css";
-
-import "@/assets/plugins/animate/animate.css";
-import "@/assets/plugins/slick/slick.css";
-import "@/assets/plugins/slick/slick-theme.css";
-import "@/assets/css/style.css";
-
-// import "@/assets/plugins/jquery/dist/jquery.min.js";
-// import "@/assets/plugins/bootstrap/js/bootstrap.min.js";
-// import "@/assets/plugins/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js";
-// import "@/assets/plugins/instafeed/instafeed.min.js";
-// import "@/assets/plugins/ekko-lightbox/dist/ekko-lightbox.min.js";
-// import "@/assets/plugins/syo-timer/build/jquery.syotimer.min.js";
-// import "@/assets/plugins/slick/slick.min.js";
-// import "@/assets/plugins/slick/slick-animation.min.js";
-// import "@/assets/js/script.js";
-
 Vue.config.productionTip = false;
 
 new Vue({
@@ -30,4 +12,3 @@ new Vue({
   store,
   render: (h) => h(App),
 }).$mount("#app");
-2;
