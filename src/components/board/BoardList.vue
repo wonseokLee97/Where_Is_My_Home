af7195092dff7020e1550baa2422f29b4ac876aa
subinan <060657@naver.com>
2022-11-19 17:50:07 +0900

    edit

--- src/components/board/BoardList.vue
+++ src/components/board/BoardList.vue
@@ -33,9 +33,9 @@ export default {
   data() {
     return {
       fields: [
-        { key: "articleno", label: "글번호", tdClass: "tdClass" },
+        { key: "articleNo", label: "글번호", tdClass: "tdClass" },
         { key: "subject", label: "제목", tdClass: "tdSubject" },
-        { key: "userid", label: "작성자", tdClass: "tdClass" },
+        { key: "userId", label: "작성자", tdClass: "tdClass" },
         { key: "regtime", label: "작성일", tdClass: "tdClass" },
         { key: "hit", label: "조회수", tdClass: "tdClass" },
       ],
